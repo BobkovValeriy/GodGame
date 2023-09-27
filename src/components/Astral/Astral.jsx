5cdab663966556f004488fd05f756c7729fbdae7
BobkovValeriy <darkmito17051990@gmail.com>
2023-09-27 17:27:00 +0300

    astral-menu-1.1

--- src/components/Astral/Astral.jsx
+++ src/components/Astral/Astral.jsx
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { changeAstralVisible } from "../../store/player/playerReducer";
 import { useTexts } from "../../locales/langagueSwitch";
 import { RiArrowGoBackLine } from "react-icons/ri";
 import ButtonWithInfo from "../Panels/infoButton/ButtonWithInfo";
 import "./Astral.scss"
+import BuildTile from "./BuildTile";
 
 
 function Astral() {
     const dispatch = useDispatch();
     const buildCardsAmount = useSelector(state => state.playerReducer.playerAstralConstructionLimits);
+    const [buildTiles, changeBuildTiles] = useState({})
     const exitButtonStyle = {
         width: "50px",
         height: "50px"
@@ -22,7 +24,12 @@ function Astral() {
         <div className="astral-wrapper">
             <div className="astral-field">
                 <div className="astral-entity-info">f</div>
-                <div className="astral-construction-field">f</div>
+                <div className="astral-construction-field">
+                    <BuildTile
+                        area={1}
+                        changeBuildTiles={changeBuildTiles}
+                    />
+                </div>
                 <div className="astral-entity-bar">
                     <ButtonWithInfo
                         buttonEvent={showAstral}
---
+++ src/components/Astral/BuildArray.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+function BuildArray() {
+
+
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+export default BuildArray;
